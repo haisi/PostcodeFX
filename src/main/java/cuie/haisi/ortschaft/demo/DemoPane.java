11dd6d25cc9fce687073c7ad6158ba83153914c3
Hasan Kara <hasan.kara@students.fhnw.ch>
2019-06-01 19:12:28 +0200

    Remove 'required' and 'readOnly' property

--- src/main/java/cuie/haisi/ortschaft/demo/DemoPane.java
+++ src/main/java/cuie/haisi/ortschaft/demo/DemoPane.java
@@ -11,8 +11,6 @@ import javafx.scene.layout.VBox;
 class DemoPane extends BorderPane {
     private OrtschaftControl businessControl;
 
-    private CheckBox readOnlyBox;
-    private CheckBox mandatoryBox;
     private TextField ortField;
     private TextField plzField;
 
@@ -32,12 +30,6 @@ class DemoPane extends BorderPane {
 
         businessControl = new OrtschaftControl();
 
-        readOnlyBox = new CheckBox();
-        readOnlyBox.setSelected(false);
-
-        mandatoryBox = new CheckBox();
-        mandatoryBox.setSelected(true);
-
         ortField = new TextField();
         plzField = new TextField();
     }
@@ -47,9 +39,7 @@ class DemoPane extends BorderPane {
         VBox box = new VBox(10,
                 new Label("Business Control Properties"),
                 new Label("Ort"), ortField,
-                new Label("PLZ"), plzField,
-                new Label("readOnly"), readOnlyBox,
-                new Label("mandatory"), mandatoryBox);
+                new Label("PLZ"), plzField);
         box.setPadding(new Insets(10));
         box.setSpacing(10);
         setRight(box);
